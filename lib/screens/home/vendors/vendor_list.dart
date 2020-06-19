30b0b34c2dc4eece5eba60cdfac6b62e8a46278c
shashwat1998 <shashwat.pandey.1901@gmail.com>
2020-06-20 04:27:25 +0530

    Fix show only vendors that are available

--- lib/screens/home/vendors/vendor_list.dart
+++ lib/screens/home/vendors/vendor_list.dart
@@ -19,15 +19,20 @@ class _VendorListState extends State<VendorList> {
 
     final vendors = Provider.of<List<Vendor>>(context) ?? [];
     if(vendors == []) return Loading();
+    //vendors.forEach((element) {print(element.name);});
+
     return FutureBuilder(
       future: Operate().sort(vendors),
       builder: (BuildContext context, AsyncSnapshot<List<Vendor>> snapshot) {
         if (snapshot.data == null) return Loading();
         List<Vendor> vendorsProcessed = snapshot.data;
+        vendorsProcessed.forEach((element) {print(element.name);});
+        print(vendorsProcessed.length);
         return ListView.builder(
           itemCount: vendorsProcessed.length,
           itemBuilder: (context, index) {
-            print(vendorsProcessed[index].distance);
+            //print(vendorsProcessed[index].name + vendorsProcessed[index].isAvailable.toString());
+            //if(!vendorsProcessed[index].isAvailable) return null;
             return VendorTile(
               vendor: vendorsProcessed[index],
               selectVendor: widget.selectVendor,
--- lib/shared/operations.dart
+++ lib/shared/operations.dart
@@ -5,12 +5,17 @@ import 'package:readyuser/shared/constants.dart';
 class Operate{
   Future<List<Vendor>> sort(List<Vendor> input) async{
     var futures = <Future>[];
+    input.removeWhere((element) => !element.isAvailable);
     input.forEach((element) {
       futures.add(Geolocator().
-      distanceBetween(userLatitude, userLongitude, element.latitude, element.longitude).then((value) => element.distance = value));
+      distanceBetween(userLatitude, userLongitude, element.latitude, element.longitude).then((value) {
+        //print(element.name+value.toString());
+        return element.distance = value;
+      }));
     });
     await Future.wait(futures);
     input.sort((a, b) => a.distance.compareTo(b.distance));
+    //input.forEach((element) {print(element.name);});
     return input;
   }
 }
